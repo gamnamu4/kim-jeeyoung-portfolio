8f2be31407fd5b05314d1453cfadfb551d293f77
gamnamu4 <64887430+gamnamu4@users.noreply.github.com>
2022-03-25 19:34:01 +0900

    0325 go to top

--- portfolio/website-works/01/custom.js
+++ portfolio/website-works/01/custom.js
@@ -1,6 +1,21 @@
 $(function(){
+  // trigger
   $('.trigger').click(function(){
     $(this).toggleClass('active')
+    $('.gnb').toggleClass('active')
+  })
+  $('section, .menu a').click(function(){
+    $('.gnb').removeClass('active')
+    $('.trigger').removeClass('active')
+  })
+  // change CSS with scroll
+  $(window).scroll(function(){
+    if($(window).scrollTop() > 50) {
+      $('header, gototop').addClass('active')
+    }
+    else {
+      $('header, gototop').removeClass('active')
+    }
   })
 })
 
