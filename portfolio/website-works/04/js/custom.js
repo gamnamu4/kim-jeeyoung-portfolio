b3b6a5e14fa5788ab148c5c7022511afdb4fa69e
gamnamu4 <64887430+gamnamu4@users.noreply.github.com>
2022-07-15 17:15:49 +0900

    js

--- portfolio/website-works/04/js/custom.js
+++ portfolio/website-works/04/js/custom.js
@@ -1,10 +1,18 @@
 $(function(){
-  /* Scroll Change Download */
+  // Scroll change download
   $(window).scroll(function(){
-    if($(window).scrollTop() > 500) {
+    if($(window).scrollTop() > 500 ) {
       $('.download').addClass('active')
     }
     else {
       $('.download').removeClass('active')
     }
   })
+
+  // Sitemap accordion
+  $('.link-item-title').click(function(){
+    // $(this).next().slideToggle()
+    $(this).next().slideDown()
+    $(this).parent().siblings().children('.link-item-content').slideUp()
+  })
+})
