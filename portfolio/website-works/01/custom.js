76eae2c6c7d4dd57191af53d0d937da85c28af4c
gamnamu4 <64887430+gamnamu4@users.noreply.github.com>
2022-07-22 15:51:07 +0900

    gototop

--- portfolio/website-works/01/custom.js
+++ portfolio/website-works/01/custom.js
@@ -13,6 +13,16 @@ $(function(){
   $('.menu a').click(function(e){
     $.scrollTo(this.hash || 0, 900)
   })
+
+  // Change CSS with Scroll
+  $(window).scroll(function(){
+    if($(window).scrollTop() > 50 ) {
+      $('header').addClass('active')
+    }
+    else {
+      $('header').removeClass('active')
+    }
+  })
 })
 
 
